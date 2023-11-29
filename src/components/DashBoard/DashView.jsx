f1c14876afcfe42b441335d6d72f71f9734b2781
Abhishek Kansal <kansalabhishek39@gmail.com>
2023-11-29 13:26:03 +0530

    First Commit

--- src/components/DashBoard/DashView.jsx
+++ src/components/DashBoard/DashView.jsx
@@ -27,13 +27,13 @@ const DashView = () => {
                           className="imageContainer relative"
                           style={{ width: "15px", height: "15px", display : 'inline-block' }}
                         >
-                          <img
+                          <div
                             style={{
                               width: "100%",
                               height: "100%",
                               borderRadius: "50%",
                             }}
-                            src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnxlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80"
+                            // src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnxlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80"
                             alt="UserImage"
                           />
                         </div>
