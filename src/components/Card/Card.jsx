2907fdbd68f125888869f41976cb0cf0fef0f066
kansalabhishek07 <95756116+kansalabhishek07@users.noreply.github.com>
2023-11-29 17:40:46 +0530

    Update Card.jsx

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -5,7 +5,7 @@ const Card = ({id, title, tag, status}) => {
   return (
     <div className="cardContainer flex-gap-10" style={{gap : '5px'}}>
         <div className="cardHeading flex-sb">
-            <span style={{textTransform : "uppercase"}} className='color-grey'>{id}</span>
+            <span style={{textTransform : "uppercase"}} className='color-orange'>{id}</span>
             <div className="imageContainer relative" style={{ width : "30px", height : "30px"}}>
                 <div style={{width : "100%", height : "100%",  borderRadius : "50%" }}  />
                 <div className="showStatus"></div>
@@ -15,10 +15,10 @@ const Card = ({id, title, tag, status}) => {
             <p>{title}</p>
         </div>
         <div className="cardTags">
-        <div className="tags color-red"> ... </div>
+        <div className="tags color-orange"> ... </div>
             {
                 tag?.map((elem, index) => {
-                    return <div key={index} className="tags color-red"> <span>•</span> {elem}</div>
+                    return <div key={index} className="tags color-orange"> <span>•</span> {elem}</div>
                 })
             }
         </div>
